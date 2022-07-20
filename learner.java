f35ffd78a1f0ed746f71ac9ab9152924bf5a9770
PlentyOfFishInTheC <109455118+PlentyOfFishInTheC@users.noreply.github.com>
2022-07-20 00:14:11 -0500

    Add files via upload

--- learner.java
+++ learner.java
@@ -1,20 +1,21 @@
-import java.util.Arrays;
-class encode
+import java.util.Scanner;
+class learner
 {
     public static void main(String[] args)
     {
         String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","-----",".----","..---","...--","....-",".....","-....","--...","---..","----.","--..--",".-.-.-","/"};
         String[] latin = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9",",","."," "};
-        String buffer = new String();
-        for(String s: args)
+        Scanner keyboard = new Scanner(System.in);
+        for(int i = 0; i < morse.length; i++)
         {
-            for(int i = 0; i < s.length(); i++)
+            System.out.printf(latin[i] + " - ");
+            String in = keyboard.nextLine();
+            if(!in.equals(morse[i]))
             {
-                buffer = morse[Arrays.asList(latin).indexOf(s.split("")[i])];
-                System.out.printf(buffer);
-                if(i + 1 != s.length()) System.out.printf(" ");
+                System.out.println(in + " is incorrect. Try: " + morse[i]);
+                i--;
             }
-            System.out.printf(" / ");
         }
+        keyboard.close();
     }
 }
